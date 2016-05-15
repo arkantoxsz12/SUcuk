fa9d13d1410539b65ace4a5add404d8766e90309
Eralp Şahin <eralpsahin@sabanciuniv.edu>
2016-05-15 20:41:43 +0300

    Update CustomerActivity.java

--- app/src/main/java/com/sucuk/sucuk/CustomerActivity.java
+++ app/src/main/java/com/sucuk/sucuk/CustomerActivity.java
@@ -1,29 +1,32 @@
 package com.sucuk.sucuk;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.VideoView;
 
 public class CustomerActivity extends Activity {
 
-    ImageView btnPiazza,btnPigastro,btnInn,btnKopuklu,btnSima;
     public static int restaurantID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.MENU_NAME," hello");
+        values.put(DBOpenHelper.MENU_PRICE,22);
+        Uri uri =getContentResolver().insert(OrderProvider.CONTENT_URI,values);
+
+        Log.d("Tag","Inserted order "+uri.getLastPathSegment());
     }
     public void sendToMenu(View v)
     {
         Intent intent = new Intent(CustomerActivity.this,MenuActivity.class);
         restaurantID=v.getId();
-        intent.putExtra("Rest",v.getId());
         startActivityForResult(intent,1);
     }
     @Override
