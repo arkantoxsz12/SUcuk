a62024b2f44965bcfcc5aa1ee1642d7a252ec4de
idokurel <dokureli@gmail.com>
2016-05-15 20:37:01 +0300

    Revert "Restaurant Activity wip"
    This reverts commit e4a692ac5a42b105a29d2d395bf04658ab396106.

--- app/src/main/java/com/sucuk/sucuk/MenuAdapter.java
+++ app/src/main/java/com/sucuk/sucuk/MenuAdapter.java
@@ -14,14 +14,14 @@ import com.firebase.ui.FirebaseListAdapter;
 public class MenuAdapter extends FirebaseListAdapter<MenuItem> {
 
 
-    public MenuAdapter(Query ref, Activity activity) {
-        super(activity, MenuItem.class, R.layout.meal_list_item, ref);
+    public MenuAdapter(Query ref,Activity activity) {
+        super(activity,MenuItem.class,R.layout.meal_list_item, ref);
     }
 
     @Override
     protected void populateView(View view, MenuItem menuItem, int i) {
-        ((TextView) view.findViewById(R.id.mealPrice)).setText(Double.toString(menuItem.getPrice()));
-        ((TextView) view.findViewById(R.id.mealName)).setText(menuItem.getName());
+        ((TextView)view.findViewById(R.id.mealPrice)).setText(Double.toString(menuItem.getPrice()));
+        ((TextView)view.findViewById(R.id.mealName)).setText(menuItem.getName());
     }
 
 }
