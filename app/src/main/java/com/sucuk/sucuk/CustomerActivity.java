fb7ef6e2fd37296c7ed5a6ead347b6acc5eacabc
idokurel <dokureli@gmail.com>
2016-05-15 13:34:27 +0300

    logos and imageview redirections

--- app/src/main/java/com/sucuk/sucuk/CustomerActivity.java
+++ app/src/main/java/com/sucuk/sucuk/CustomerActivity.java
@@ -7,16 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.VideoView;
 
 public class CustomerActivity extends Activity {
 
-    Button btnPiazza,btnPigastro;
+    ImageView btnPiazza,btnPigastro,btnInn,btnKopuklu,btnSima;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
-        btnPiazza= (Button) findViewById(R.id.btnPiazza);
+        btnPiazza= (ImageView) findViewById(R.id.imgpiazza);
         btnPiazza.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -26,12 +27,40 @@ public class CustomerActivity extends Activity {
             }
         });
 
-        btnPigastro= (Button) findViewById(R.id.btnPigastro);
+        btnPigastro= (ImageView) findViewById(R.id.imgpigastro);
         btnPigastro.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CustomerActivity.this,TrialActivity.class);
                 intent.putExtra("Rest","Pigastro");
+                startActivityForResult(intent,1);
+            }
+        });
+        btnInn= (ImageView) findViewById(R.id.imginncafe);
+        btnInn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CustomerActivity.this,TrialActivity.class);
+                intent.putExtra("Rest","Inncafe");
+                startActivityForResult(intent,1);
+            }
+        });
+
+        btnKopuklu= (ImageView) findViewById(R.id.imgkopuklu);
+        btnKopuklu.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CustomerActivity.this,TrialActivity.class);
+                intent.putExtra("Rest","Kopuklu");
+                startActivityForResult(intent,1);
+            }
+        });
+        btnSima= (ImageView) findViewById(R.id.imgsima);
+        btnSima.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CustomerActivity.this,TrialActivity.class);
+                intent.putExtra("Rest","Sima");
                 startActivityForResult(intent,1);
             }
         });
